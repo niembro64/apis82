eb84e62ef1a0dc2cd97260a8e2c3ed401d8a1c13
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-15 12:31:28 -0400

    showing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+  const [breweries, setBreweries] = useState([]);
+
   const fetchInfo = () => {
     console.log("get info calling api");
     fetch("https://api.openbrewerydb.org/breweries?by_city=cleveland")
       .then((res) => res.json())
       .then((res) => {
         console.log(res);
+        setBreweries(res);
       })
       .catch((err) => console.log(err));
   };
@@ -18,6 +22,11 @@ function App() {
       <button onClick={fetchInfo} className="btn btn-primary btn-lg">
         fetch info
       </button>
+      <ul>
+        {breweries.map((item, i) => {
+          return <li key={i}>{item.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
